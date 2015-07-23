9b9bb1e6d1e48cf9214b7cb894d6ff608ad331aa
thomas <thomas.jomeau@gmail.com>
2015-07-23 10:18:54 +0200

    correction controler mail

--- controller/mail/create.php
+++ controller/mail/create.php
@@ -1,4 +1,6 @@
+<?php
 $lead             = "BIENVENUE SUR LA MAQUETTE DE RESERVATION";
 $tagline          = "Ici vous avez la possibilité de vous inscrire aux évenements.";
 $breadcrumbs      = array("mail");
-$pageInclude      = "evenement/createMail.php";
+$pageInclude      = "mail/createMail.php";
+?>
