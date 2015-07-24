38a14f6ba91377693030974772630606f9a5397d
phetmixay <pphetmixay@gmail.com>
2015-07-24 15:51:14 +0200

    Mise en forme événement
    Séance du XX/XX/XX

--- vue/evenement/listEvenement.php
+++ vue/evenement/listEvenement.php
@@ -3,7 +3,7 @@
     <?php $datetimEvenement = new DateTime($evenement['ev_date']); ?>
     <div class="col-md-12">
       <div class="well">
-          <h2><?= ucfirst(lcfirst($evenement['ev_libelle'])); ?></h2>
+          <h2>Séance du <?= $datetimEvenement->format('d / m / Y') ?> à <?= $datetimEvenement->format('H:i') ?></h2>
           <span class="label label-info">
           <?= $evenement['nb_cat']; ?>
           <?php if ($evenement['nb_cat'] == 1): ?>
@@ -15,7 +15,6 @@
           <?php if ($evenement['facture']): ?>
             <span class="label label-warning">Deja inscrit</span>
           <?php endif ?>
-          <p>Séance du <?= $datetimEvenement->format('d / m / Y') ?> à <?= $datetimEvenement->format('H:i') ?><p>
           <p>Salle <?= $evenement['lieu_nomSalle']; ?></p>
           <a href="index.php?route=showEvenement&id=<?= $evenement['ev_id']; ?>" role="button">
             En savoir plus!
