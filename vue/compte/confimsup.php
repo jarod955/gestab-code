d8258d1295bff1c2c3e2b87148586a8df25667e0
Aymeric <aymericboucher78@gmail.com>
2015-07-26 21:48:28 +0200

    confirm supp

---
+++ vue/compte/confimsup.php
@@ -0,0 +1,18 @@
+<div class="container">
+<div class="well">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="form-group">
+              <label for="prenom"  class="col-sm-4 control-label"><h2>etes vous sur de vouloir supprimer votre compte ?</h2></label>
+              <div class="col-sm-4">
+              </div>
+              <div class="col-sm-4">
+                <a href="index.php?route=supprUtilisateurs" type="button" class="btn btn-primary"><h2>OUI</h2></a>
+                <a href="index.php?route=gestionUtilisateurs" type="button" class="btn btn-primary"  ><h2>NON</h2></a>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+</div>
