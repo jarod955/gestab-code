91a547da972c697f5fb7f49209d6d5a87aeb6cac
thibaud <thibaud.vernichon@gmail.com>
2015-07-26 17:13:52 +0200

    Reindentation du code et mdp minimum 8 caract

--- controller/connexion/inscription.php
+++ controller/connexion/inscription.php
@@ -29,119 +29,141 @@
             //$erreur = "Les champs marqué d'un * sont obligatoire";
              error("Les champs marqué d'un * sont obligatoire"); 
         }
-            else
+        else
+        {
+
+                #[^0-9]#
+            if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $email))
             {
-                if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#", $email))
+                if ((strlen($nom) >= 30) || (strlen($prenom) >= 30) || (strlen($rue) >= 30) || (strlen($ville) >= 30) || (strlen($email) >= 30))
                 {
-                    if ((strlen($nom) >= 30) || (strlen($prenom) >= 30) || (strlen($rue) >= 30) || (strlen($ville) >= 30) || (strlen($email) >= 30))
-                    {
                     //$erreur = 'Un des champs depasse la limite de 30 charactere';
-                     error("Un des champs depasse la limite de 30 characteres"); 
-                    }
-                        else
-                        {
-                            if ($pass_hache != $pass_hache2)
-                            {
-                            //$erreur = 'le mot de passe et le mot de passe de confirmation ne correspondent pas ';
-                                 error("le mot de passe et le mot de passe de confirmation ne correspondent pas!"); 
-                            }
-                                else
-                                {
-                                    if ($email != $email2)
-                                    {
-                                    //$erreur = 'les emails rentré ne sont pas identique ';
-                                         error("les emails rentré ne sont pas identique"); 
-                                    }
-                                        else
-                                        {
-                                            if (preg_match("#^0[1-78]([-. ]?[0-9]{2}){4}$#", ($telephone)))
-                                            {
-                                            $sql = $bdd->prepare('SELECT inter_mail FROM internaute WHERE inter_mail = \''.$email.'\';');
-                                            $sql->execute(array('.$email.' => $_POST['email']));
-                                            $res = $sql->fetch();
-
-                                                if ($res)
-                                                {
-                                                //$erreur = "Cet email est déjà utilisé !";
-                                                     error("Cet email est déjà utilisé !"); 
-                                                }
-                                                    else
-                                                    {
-                                                        
-
-                                                        if (is_numeric($numrue) && is_numeric($codepostal))
-                                                        {
-                                                        //$erreur = "Saisissez une valeur numérique !";
-                                                           
-                                                        
-                                                            
-                                                               
-                                                             
-                                                                
-                                                            if (empty($entite) && empty($numrueentite) && empty($rueentite) && empty($codepostalentite) && empty($villeentite))
-                                                            {
-
-                                                                addAdresse($bdd, $numrue, $rue, $ville, $codepostal);
-                                                                $adrid= $bdd->lastInsertId();
-
-                                                                    
-                                                                    /*addInternaute($bdd, 1, $nom, $prenom, NULL, $adrid, $pass_hache, $email, $telephone);*/
-                                                                addInternaute($bdd, $nom, $prenom, $adrid, $pass_hache, $email, $telephone);
-                                                                    
-                                                                    //$succes   = '<strong>Félicitation!</strong> le compte a bien été créé';
-                                                                success("<strong>Félicitation!</strong> le compte a bien été créé."); 
-                                                                }
-                                                            else 
-                                                            {
-                                                                addAdresse($bdd, $numrue, $rue, $ville, $codepostal);
-                                                                $adrid= $bdd->lastInsertId();
-                                                                    
-
-                                                                    
-                                                                addAdresse($bdd, $numrueentite, $rueentite, $villeentite, $codepostalentite);
-                                                                $adride= $bdd->lastInsertId();
-                                                                    
-                                                                    
-                                                                addEntite($bdd, $entite, $adride);
-                                                                $entiteid= $bdd->lastInsertId();
-                                                                    
-                                                                    
-                                                                addInternautee($bdd, $nom, $prenom, $entiteid, $adrid, $pass_hache, $email, $telephone);
-                                                                    //$succes   = '<strong>Félicitation!</strong> le compte a bien été créé avec votre entité.';
-                                                                success("<strong>Félicitation!</strong> le compte a bien été créé avec votre entité."); 
-                                                            }
-
-                                                                
-                                                              
-                                                                    
-                                                        }
-                                                        else
-                                                        { 
-                                                            error("Le numero de la rue et/ou le code postal doit etre de type numerique !"); 
-                                                        }
-                                                    }
-                                                }
-                                                else
-                                                {
-                                            //$erreur = "Merci d'écrire un numero de telephone français ou valide";
-                                                    error("Merci d'écrire un numero de telephone français ou valide");
-                                                }   
-                                        }
-                                }
-                        }
+                    error("Un des champs depasse la limite de 30 characteres"); 
                 }
                 else
                 {
-                //$erreur = "Merci d'écrire une adresse email valide !";
-                 ?><?php error("Merci d'écrire une adresse email valide !"); ?><?php
+
+                    if ($pass_hache != $pass_hache2)
+                    {
+                            //$erreur = 'le mot de passe et le mot de passe de confirmation ne correspondent pas ';
+                        error("le mot de passe et le mot de passe de confirmation ne correspondent pas!"); 
+                    }
+                    else
+                    {
+                        if (strlen($nom) < 8)
+                        {
+                            error("le mot de passe doit contenir au moins 8 caracteres!"); 
+                        }
+                        else
+                        {
+
+
+                            if ($email != $email2)
+                            {
+                                    //$erreur = 'les emails rentré ne sont pas identique ';
+                                error("les emails rentré ne sont pas identique"); 
+                            }
+                            else
+                            {
+                                if (preg_match("#^0[1-78]([-. ]?[0-9]{2}){4}$#", ($telephone)))
+                                {
+                                    $sql = $bdd->prepare('SELECT inter_mail FROM internaute WHERE inter_mail = \''.$email.'\';');
+                                    $sql->execute(array('.$email.' => $_POST['email']));
+                                    $res = $sql->fetch();
+
+                                    if ($res)
+                                    {
+                                                //$erreur = "Cet email est déjà utilisé !";
+                                        error("Cet email est déjà utilisé !"); 
+                                    }
+                                    else
+                                    {
+                                                
+
+                                        if (is_numeric($numrue) && is_numeric($codepostal))
+                                        {
+                                                        //$erreur = "Saisissez une valeur numérique !";
+                                                           
+                                                            
+                                                            /*if (preg_match("#[^0-9]#", $nom))*/
+                                            $pattern = '#[^0-9]#';
+                                            if (preg_match($pattern, $nom) && preg_match($pattern, $prenom) && preg_match($pattern, $rue))
+                                            {
+                                                                
+                                                if (empty($entite) && empty($numrueentite) && empty($rueentite) && empty($codepostalentite) && empty($villeentite))
+                                                {
+
+                                                    addAdresse($bdd, $numrue, $rue, $ville, $codepostal);
+                                                    $adrid= $bdd->lastInsertId();
+
+                                                                    
+                                                                    /*addInternaute($bdd, 1, $nom, $prenom, NULL, $adrid, $pass_hache, $email, $telephone);*/
+                                                    addInternaute($bdd, $nom, $prenom, $adrid, $pass_hache, $email, $telephone);
+                                                                    
+                                                                    //$succes   = '<strong>Félicitation!</strong> le compte a bien été créé';
+                                                    success("<strong>Félicitation!</strong> le compte a bien été créé."); 
+                                                }
+                                                else 
+                                                {
+                                                    if (is_numeric($numrueentite) && is_numeric($codepostalentite))
+                                                    {
+                                                        addAdresse($bdd, $numrue, $rue, $ville, $codepostal);
+                                                        $adrid= $bdd->lastInsertId();
+                                                                    
+                                                        addAdresse($bdd, $numrueentite, $rueentite, $villeentite, $codepostalentite);
+                                                        $adride= $bdd->lastInsertId();
+                                                                    
+                                                        addEntite($bdd, $entite, $adride);
+                                                        $entiteid= $bdd->lastInsertId();
+                                                                    
+                                                        addInternautee($bdd, $nom, $prenom, $entiteid, $adrid, $pass_hache, $email, $telephone);
+                                                                    //$succes   = '<strong>Félicitation!</strong> le compte a bien été créé avec votre entité.';
+                                                        success("<strong>Félicitation!</strong> le compte a bien été créé avec votre entité."); 
+                                                    }
+                                                    else
+                                                    {
+                                                        error("Le numero de la rue et/ou le code postal doit etre de type numerique pour entité !"); 
+                                                    }
+                                                }
+
+                                                            
+                                            }   
+                                            else
+                                            {
+                                                error("Le nom et/ou prenom et/ou ville doivent etre de type charactere");
+                                            }
+                                                                    
+                                        }
+                                        else
+                                        { 
+                                            error("Le numero de la rue et/ou le code postal doit etre de type numerique !"); 
+                                        }
+
+                                    }
+
+                                }
+                                else
+                                {
+                                            //$erreur = "Merci d'écrire un numero de telephone français ou valide";
+                                    error("Merci d'écrire un numero de telephone français ou valide");
+                                }   
+                            } 
+
+                        }
+
+                    }
+
                 }
 
-        // }
-        // else
-        // {
-        //     $erreur = "Merci d'écrire un numrue/codepostal valide !";
-        // }
             }
+            else
+            {
+                //$erreur = "Merci d'écrire une adresse email valide !";
+                error("Merci d'écrire une adresse email valide !"); 
+            }
+
+        }
+
     }
 
 	$lead        = "Inscription";
