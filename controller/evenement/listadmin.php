bf8106bb6e4f04b24de1a1a628a526535cb9b595
thomas <thomas.jomeau@gmail.com>
2015-07-27 14:35:04 +0200

    securite page listadminEvenement : niveau 3 :+1:

--- controller/evenement/listadmin.php
+++ controller/evenement/listadmin.php
@@ -1,4 +1,9 @@
-<?php
+<?php 
+if (empty($_SESSION['user'])){
+	redirection($page = "index.php?route=error404");
+}
+elseif ($_SESSION['user']['inter_stat_id'] == 1 or 2 or 3) {
+ // Mettre ici tout le corps de la page 
 	include('model/categorie.php');
 	include('model/internaute.php');
 	$evenements  = listModel($bdd);
@@ -6,4 +11,12 @@
 	$tagline     = "Ici vous avez la possibilité de modifier les évenements.";
 	$breadcrumbs = array("list");
 	$pageInclude = "evenement/listadminEvenement.php";
- ?>
+ 
+}
+else{
+ 
+redirection($page = "index.php?");
+}
+?>
+
+	
