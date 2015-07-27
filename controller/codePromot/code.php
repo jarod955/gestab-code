54622ae88eb63768ffd3e3f0771fe0add97d3081
thomas <thomas.jomeau@gmail.com>
2015-07-27 15:11:02 +0200

    securite codePromot N1 N2 N3

--- controller/codePromot/code.php
+++ controller/codePromot/code.php
@@ -2,7 +2,7 @@
 if (empty($_SESSION['user'])){
     header('location: error.php');
 }
-elseif ($_SESSION['user']['inter_stat_id'] == 2 or 3) {
+elseif ($_SESSION['user']['inter_stat_id'] == 1 or 2 or 3) {
  // Mettre ici tout le corps de la page 
  include('model/evenement.php');
 
