3b1dc2cab8b2e13b92d9f784f7e0bfade3aea038
thomas <thomas.jomeau@gmail.com>
2015-07-27 12:04:04 +0200

    correction redirection promo/code.php

--- controller/promo/code.php
+++ controller/promo/code.php
@@ -1,6 +1,6 @@
 <?php 
 if (empty($_SESSION['user'])){
-    header('location: error.php');
+	redirection($page = "index.php?route=error404");
 }
 elseif ($_SESSION['user']['inter_stat_id'] == 2 or 3) {
  // Mettre ici tout le corps de la page 
@@ -13,13 +13,6 @@ elseif ($_SESSION['user']['inter_stat_id'] == 2 or 3) {
 }
 else{
  
-redirection($page = "http://localhost/gestab-code/index.php?");
+redirection($page = "index.php?");
 }
-?>
-<?php
-	
-
-
-
-
 ?>
